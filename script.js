bbda2b0b06f97148c0726ce2661380ed586efa49
Nirdosh2004 <nirdoshkushwaha75@gmail.com>
2024-07-18 17:21:21 +0530

    done

--- script.js
+++ script.js
@@ -2,3 +2,19 @@
 //           el: document.querySelector('.main'),
 //           smooth: true
 //       });
+
+
+
+function sendMail(){
+          alert("Sorry! Currently its under maintenace by Nirdosh Kushwaha");
+          // var params = {
+          //           from_name: document.getElementById("fullName").value,
+          //           email_id : document.getElementById("email_id").value,
+          //           message : document.getElementById("message").value,
+          //       }
+          //       emailjs.send('0HbJPUy7TLP7RnrtP','template_fcvcmft', params).then(function(response) {
+          //          alert('SUCCESS!', response.status, response.text);
+          //       }, function(err) {
+          //          console.log('FAILED...', err);
+          //       });
+}
